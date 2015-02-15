7998756c9b81ce28b32f1398875b2e7c96730ab4
Tom Yeh <doubleshow@gmail.com>
2015-02-15 16:55:11 -0700

    get port working

--- app.js
+++ app.js
@@ -31,10 +31,9 @@ require('./routes/instructorView')(app)
 
 app.set('port', (process.env.PORT || 3000))
 
-var server = app.listen(3000, function() {
+var server = app.listen(app.get('port'), function() {
 
     var host = server.address().address
     var port = server.address().port
-
     console.log('App listening at http://%s:%s', host, port)
 })
