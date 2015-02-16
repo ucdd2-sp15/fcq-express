8f95e8f533ec9ac8f52d5ad64d506dc6d3854ea8
Your Name <Tahani@rgnt2-119-75-dhcp.int.colorado.edu>
2015-02-16 16:54:05 -0700

    fixed #4

--- routes/courseListName.js
+++ routes/courseListName.js
@@ -6,16 +6,18 @@ module.exports = function(app) {
         var courses = app.db.get('courses')
 
         // compose a query
-        var q = {}  // TODO
+        var q = {'Subject': (req.params.subject),'Course': (req.params.number) } 
 
         // execute the query to find those matched limiting to 20
         courses.find(q, {
-            limit: 20
+            limit: 100
         }, function(err, docs) {
 
             res.render('course/list.jade', {
                 courses: docs
+
             })
+            console.log(docs)
         })
     })
 
