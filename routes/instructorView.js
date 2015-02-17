11423e6c1eb3c380ae59be0d97ed8a57a15c66c9
Haoran Fang <gougou9903@gmail.com>
2015-02-16 17:08:45 -0700

    Fixed issue #9

--- routes/instructorView.js
+++ routes/instructorView.js
@@ -1,18 +1,33 @@
 module.exports = function(app) {
 
-    app.get('/instructor/view/:name', function(req, res) {
+    app.get('/view/instructor/:name', function(req, res) {
 
         // get the courses collection
         var courses = app.db.get('instructors')
 
-        var name = 'DANIEL_DEASY' // <--- req.params.name
+        var name = req.params.name.replace("%20", " ") // <--- req.params.name
 
-        var q = {'name': 'DEASY, DANIEL'}
+        var q = {'name': name}
 
         // execute the query to find those matched limiting to 20
         courses.findOne(q, function(err, doc) {
             
             res.render('instructor/view.jade', {
+                item: doc
+            })
+        })
+    })
+
+    app.get('/course/list/instructor/:name', function(req,res){
+
+        var courses = app.db.get('instructors')
+
+        var name = req.params.name.replace("%20", " ")
+        
+        var q = {'name': name}
+
+        courses.findOne(q, function(err, doc){
+            res.render('instructor/view.jade',{
                 item: doc
             })
         })
